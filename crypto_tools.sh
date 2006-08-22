0748c0138904f4dea6d110c2c0f76763442d0453
Colin Watson <cjwatson@debian.org>
2006-08-22 07:16:37 +0000

    * Use /dev/loop$n if the /dev/loop directory doesn't exist.
    r40122

--- crypto_tools.sh
+++ crypto_tools.sh
@@ -75,7 +75,11 @@ get_free_loop () {
 
 	for n in 0 1 2 3 4 5 6 7; do
 		if eval [ -z "\$loop$n" ]; then
-			echo /dev/loop/$n
+			if [ -d /dev/loop ]; then
+				echo /dev/loop/$n
+			else
+				echo /dev/loop$n
+			fi
 			break
 		fi
 	done

104402d5abc450e1a9ec0479befe774a4cd45d82
David Härdeman <alphix-guest@users.alioth.debian.org>
2006-12-12 20:39:02 +0000

    Completely remove scary warnings per discussions on debian-boot
    r43325

--- crypto_tools.sh
+++ crypto_tools.sh
@@ -619,17 +619,6 @@ $desc $value"
 }
 
 crypto_check_setup() {
-	# Show warning before anything else
-	#
-	# NOTE: Only commented out because we might want to reenable this post-Etch
-	#templ="partman-crypto/warning_experimental_nonaudit"
-	#db_input critical $templ
-	#db_go || true
-	#db_get $templ || RET=''
-	#if [ "$RET" != true ]; then
-	#	return 1
-	#fi
-
 	crypt=
 	for dev in $DEVICES/*; do
 		[ -d "$dev" ] || continue

bc45104fb788a4f87c673f4784a0d934ba1a9fb1
Cyril Brulebois <kibi@debian.org>
2013-10-14 11:31:00 +0200

    Replace ISO-8859 with UTF-8.

--- blockdev-wipe/blockdev-wipe.c
+++ blockdev-wipe/blockdev-wipe.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006 David Härdeman <david@2gen.com>
+ * Copyright (C) 2006 David HÃ¤rdeman <david@2gen.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
